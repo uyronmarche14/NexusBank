402c603a11dd520bb43cbea61557f67f237aef7a
Ron Marche Rhyss Q. Uy <you@example.com>
2025-03-06 17:28:06 +0800

    first commit

---
+++ app/components/services/grid_services/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NexusBankServices } from "@/data/serviceData/page";
+
+export const Services = () => {
+  return (
+    <div className="w-full py-8 flex items-center justify-center">
+      {}
+      <div className="grid grid-cols-3 w-full h-[600px] gap-4">
+        <div className=" h-[300px] w-full shadow-2xl shadow-gray-800 border-0.5 border-white rounded-2xl"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
